bf1c47107682e041397269279a87f5ccfd4be57f
Jai V. Kejriwal <jai.kejriwal@gmail.com>
2024-11-10 21:29:13 +0530

    clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 //import Image from "next/image";
-import { Main, Section, Container, Box } from "@/components/craft";
+import { Main, Section, Container } from "@/components/craft";
 import Hero1 from "@/components/home-page/hero1";
-import { NavBar } from "@/components/navbar";
 
 export default function Page() {
   return (
